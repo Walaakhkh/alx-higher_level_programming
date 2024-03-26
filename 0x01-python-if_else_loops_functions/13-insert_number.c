d82b842425f47f6369fe43b4781d09069b03fe73
Walaakhkh <Walaakhalil98@gmail.com>
2024-03-26 03:00:01 -0700

    conditional statments

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_t *insert_node - function in C that inserts a number into a
+ * sorted singly linked list
+ * @number: number to be sorted
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
