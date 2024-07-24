be94a44a22c5c841c2f561efdc2f2cfcf2bb38de
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-23 19:51:53 -0700

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Create_table 'second_table' in database 'hbtn_0c_0'
+-- Add-PIPOS (id INT), (name VAR-CHAR(256)), (score INT)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+-- Script should create maximum of 4 records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alexx", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
