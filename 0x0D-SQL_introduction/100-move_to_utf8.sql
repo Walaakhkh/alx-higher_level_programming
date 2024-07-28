0434b073ac3cef35437b4e3878f700c885c8105b
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-28 02:58:12 -0700

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,5 +7,6 @@ USE hbtn_0c_0;
 -- Change the table character set and collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Change the character set and collation of the 'name' column
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change the collation of the 'name' column
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+
