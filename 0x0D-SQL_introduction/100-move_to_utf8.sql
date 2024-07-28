9472f551167706b7a5b8b35725ad6f28def32787
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-28 03:06:26 -0700

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,4 @@
--- Change the database character set and collation
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Select the database
-USE hbtn_0c_0;
-
--- Change the table character set and collation
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Change the collation of the 'name' column
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
-
+-- Converts the entire database hbtn_0c_0 to UTFS
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
