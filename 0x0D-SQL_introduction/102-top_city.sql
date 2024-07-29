e74fc5650a73d177c2511672f6e493b062d9be20
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-29 07:08:10 -0700

    task 19

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- July and August
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE `month` = 7 OR `month` = 3
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
