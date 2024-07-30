6ac73921870c10d7c5633c70a212ec0ca1abadbf
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-30 10:33:47 -0700

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// 3-rectangle.js
+class Rectangle {
+  constructor(w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
