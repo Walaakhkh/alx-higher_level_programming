873cde3a9f5f2806464c99f9efc0124baffb1cf7
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-30 08:50:57 -0700

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Define the recursive factorial function
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+// Get the first argument passed to the script
+const arg = process.argv[2];
+
+// Convert the argument to an integer
+const num = parseInt(arg);
+
+// Compute and print the factorial
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
