3042b33fb245c24a3ac30e76ef8d48f5f43e0809
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-30 08:59:08 -0700

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ function factorial (n) {
   if (n === 0 || n === 1) {
     return 1;
   }
-	return n * factorial (n - 1);
+  return n * factorial(n - 1);
 }
 
 // Get the first argument passed to the script
