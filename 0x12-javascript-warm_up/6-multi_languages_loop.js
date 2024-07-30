5b5b9e066c2b09699e6e7c086b98e907a8786f37
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-30 08:33:57 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Array of strings to print
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each message
+for (const message of messages) {
+  console.log(message);
+}
