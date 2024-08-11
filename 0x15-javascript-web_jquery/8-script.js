559295f68a48c5ba95bfb17e2e5fa91855d672f4
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-11 04:06:22 -0700

    task 8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,7 @@
 /* global $ */
-$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
-    const movies = data.results;
-    movies.forEach(movie => {
-        $('#list_movies').append('<li>' + movie.title + '</li>');
-    });
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  const movies = data.results;
+  movies.forEach(movie => {
+    $('#list_movies').append('<li>' + movie.title + '</li>');
+  });
 });
