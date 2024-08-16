a3d413f3d92e25ff88b80c9bf4acd2750cd92d48
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-16 05:39:44 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,7 +16,7 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a new Square instance with the given size.
-    
+
         Args:
             size (int): The size of the square, must be an integer >= 0.
 
