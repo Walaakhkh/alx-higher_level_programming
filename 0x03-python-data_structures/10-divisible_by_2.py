d8910cabf1cc78a2c8f7eb6268729cdbce9f467b
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-16 03:58:18 -0700

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
+
 def divisible_by_2(my_list=[]):
-    length =len(my_list)
-    new_list = []
-    for i in range(length):
-        if my_list[i] % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-            return new_list
+    # Use a list comprehension to create a new list of boolean values
+    return [True if number % 2 == 0 else False for number in my_list]
