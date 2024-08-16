488e0ca078ef0c2d53e87e14a810ecfef3ba5129
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-16 03:33:05 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 import sys
+
 
 def main():
     # sys.argv contains the script name and all command-line arguments
     # sys.argv[1:] skips the script name itself
     total_sum = sum(int(arg) for arg in sys.argv[1:])
     print(total_sum)
+
 
 if __name__ == "__main__":
     main()
