9b8325f85a089812d1e49d964084b890f93aced1
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-18 08:34:54 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end='')
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The path to the file to be read. Defaults to an empty
+        string.
+
+    This function uses the `with` statement to open and read the file
+    to ensure proper resource management.
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            print(file.read(), end='')
+    except FileNotFoundError:
+        pass
