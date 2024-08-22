c14d7deffa93c500e95f34cdd4c9207772def258
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-22 12:10:01 -0700

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -10,33 +10,42 @@ def lookup(obj):
     """
     return dir(obj)
 
+
 # Test with built-in types
 print(lookup(int))       # Should list int methods and attributes
 print(lookup(float))     # Should list float methods and attributes
 print(lookup(list))      # Should list list methods and attributes
+
 
 # Test with a class with a custom __dir__ method
 class MyClass:
     def __dir__(self):
         return ["my_class", "is", "empty"]
 
+
 print(lookup(MyClass()))  # Should list ["my_class", "is", "empty"]
+
 
 # Test with a basic class
 class MyClass:
     pass
 
+
 print(lookup(MyClass))    # Should list default attributes and methods
+
 
 # Test with a class with one attribute
 class MyClass:
     one_attribute = 89
 
+
 print(lookup(MyClass))    # Should include 'one_attribute'
+
 
 # Test with a class with one method
 class MyClass:
     def one_meth(self):
         pass
+
 
 print(lookup(MyClass))    # Should include 'one_meth'
