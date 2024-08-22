af8a805fc2094a9fac615650a19a037c970a4f61
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-22 12:30:16 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-'''Module for MyList class'''
+'''Module for MyList class.'''
 
 
 class MyList(list):
-    '''Custom MyList class'''
-    def print_stored(self):
-        '''Method for printing stored list'''
-        print(stored(self))
+    '''Custom MyList class.'''
+    def print_sorted(self):
+        '''Method for printing sorted list.'''
+        print(sorted(self))
