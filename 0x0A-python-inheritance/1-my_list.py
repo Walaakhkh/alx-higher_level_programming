07170aa2755f43562c6a8e7c167e65eb6c151edc
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-22 12:26:22 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for MyList class'''
+
+
+class MyList(list):
+    '''Custom MyList class'''
+    def print_stored(self):
+        '''Method for printing stored list'''
+        print(stored(self))
