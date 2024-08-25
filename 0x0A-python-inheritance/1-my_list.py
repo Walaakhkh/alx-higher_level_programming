0b509c8074af1cac575cbba89fd84f94dbf96563
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-25 05:51:21 -0700

    task 2

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-class MyList(list):
-    """Class that inherits from list with an additional method to
-    print the list sorted."""
-
-    def print_sorted(self):
-        """Prints the list in ascending order."""
-        print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-'''Module for is_same_class method'''
+'''Module for is_same_class method.'''
 
 
 def is_same_class(obj, a_class):
-     """
-    Returns True if the object is exactly an instance of the specified class;
-    otherwise False.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to match the type of obj to.
-
-    Returns:
-        True if obj is exactly an instance of a_class, otherwise False.
-    """
+    '''Determines if an object is exactly an instance of a class.'''
     return type(obj) == a_class
