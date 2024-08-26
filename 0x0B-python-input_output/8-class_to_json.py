f2d3e5baf5c5252cd767da1955a822e9ffed31d1
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-26 02:44:30 -0700

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
+ """ Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON
-    serialization of an object."""
     # Dictionary to hold serializable attributes
     result = {}
 
