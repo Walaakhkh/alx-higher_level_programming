c2b1b3c746d2a83557fa37bc5616d80cf6af6db7
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-26 03:16:27 -0700

    task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -15,7 +15,7 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename, 'r') as file:
         lines = file.readlines()
-    
+
     with open(filename, 'w') as file:
         for line in lines:
             file.write(line)
