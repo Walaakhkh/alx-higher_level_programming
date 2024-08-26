f0c9cb949424d7bad8209d742096716a31983a2d
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-26 02:27:31 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,7 @@ to a file"""
 import sys
 import importlib.util
 
+
 # Function to dynamically load a module from a given filename
 def load_module_from_file(module_name, filename):
     spec = importlib.util.spec_from_file_location(module_name, filename)
@@ -16,11 +17,15 @@ def load_module_from_file(module_name, filename):
 
 def main():
     filename = 'add_item.json'
-    
+
     # Load the save_to_json_file and load_from_json_file functions
-    save_to_json_file_module = load_module_from_file('save_to_json_file', '5-save_to_json_file.py')
-    load_from_json_file_module = load_module_from_file('load_from_json_file', '6-load_from_json_file.py')
-    
+    save_to_json_file_module = load_module_from_file(
+            'save_to_json_file', '5-save_to_json_file.py'
+            )
+    load_from_json_file_module = load_module_from_file(
+            'load_from_json_file', '6-load_from_json_file.py'
+            )
+
     save_to_json_file = save_to_json_file_module.save_to_json_file
     load_from_json_file = load_from_json_file_module.load_from_json_file
 
@@ -29,12 +34,13 @@ def main():
         items_list = load_from_json_file(filename)
     except FileNotFoundError:
         items_list = []
-    
+
     # Add command line arguments to the list (excluding the script name)
     items_list.extend(sys.argv[1:])
-    
+
     # Save the updated list to the file
     save_to_json_file(items_list, filename)
+
 
 if __name__ == "__main__":
     main()
