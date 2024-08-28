baaadfe90b8c646f506381dcaeed843542e3c119
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 13:18:06 -0700

    task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X GET 0.0.0.0:5000/catch_me -H "User-Agent: HolbertonSchool" -b "user_id=98"
+# Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the respones
+curl -sL -X POST 0.0.0.0:5000/catch_me -H "Content-Type: application/json" -d '{"user_id": "98"}'
