5efc8e77c466aa57d79aaf05471cd694a48ac122
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 12:17:41 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 
 def find_peak(list_of_integers):
     """Finds a peak element in a list of unsorted integers"""
-    
+
     def binary_peak(start, end):
         """Helper function to perform binary search for a peak"""
         if start == end:
@@ -13,9 +13,9 @@ def find_peak(list_of_integers):
         mid = (start + end) // 2
 
         # Check if mid is a peak
-        if ((mid == 0 or 
+        if ((mid == 0 or
              list_of_integers[mid] >= list_of_integers[mid - 1]) and
-            (mid == len(list_of_integers) - 1 or 
+            (mid == len(list_of_integers) - 1 or
              list_of_integers[mid] >= list_of_integers[mid + 1])):
             return list_of_integers[mid]
 
