1cce0a733ab7747e6293a5e568d37d0a3d082720
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 13:15:43 -0700

    task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X GET 0.0.0.0:5000/catch_me --max-redirs 10 -H "User-Agent: HolbertonSchool" -b "user_id=98"
+curl -sL -X GET 0.0.0.0:5000/catch_me -H "User-Agent: HolbertonSchool" -b "user_id=98"
