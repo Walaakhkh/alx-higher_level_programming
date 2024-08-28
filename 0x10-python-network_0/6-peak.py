a4f22ee3a49f995cf5e179108f5ec5ddedca3277
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 11:57:38 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Write a function that finds a peak in a list of unsorted integers"""
 
+
 def find_peak(list_of_integers):
     """" Function that finds a peak in the shortest algorithm"""
     if list_of_integers == []:
