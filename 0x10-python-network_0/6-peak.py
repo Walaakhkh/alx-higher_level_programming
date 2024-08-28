2883331a9627af7d77fef804df53cda048bc42cb
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 12:14:35 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,9 +12,10 @@ def find_peak(list_of_integers):
         mid = (start + end) // 2
 
         # Check if mid is a peak
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-                (mid == len(list_of_integers) - 1
-                   or list_of_integers[mid] >= list_of_integers[mid + 1]):
+        if (mid == 0 or
+            list_of_integers[mid] >= list_of_integers[mid - 1]) and
+            (mid == len(list_of_integers) - 1 or
+            list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
 
         if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
