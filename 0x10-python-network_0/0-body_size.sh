739191b8ea5b644490ffffed37f2c2723fd34653
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 10:59:56 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 # Check if exactly one argument is provided
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <URL>"
