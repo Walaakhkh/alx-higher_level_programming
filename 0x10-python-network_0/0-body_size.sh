926872b6e94e6a3fd7f69739a535a022896791db
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 11:02:46 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# Check if exactly one argument is provided
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-URL="$1"
-
-# Send a request to the URL and display the size of the response body in bytes
-curl -sI "$URL" | grep -i 'Content-Length:' | awk '{print $2}' | tr -d '\r'
+# Script that displays the size of the body of the response
+curl -s "$1" | wc -c
