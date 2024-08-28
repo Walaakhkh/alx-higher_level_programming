5093564c53687b35840935828a4a07f8f3170717
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 11:23:08 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL and displays the body if status code is 200
-curl -s "$1"
+# This script sends a GET request to a URL and displays the body if status code is 200, handling redirects
+curl -s -L -w "%{http_code}" -o /tmp/response_body.txt "$1" | grep -q '200' && cat /tmp/response_body.txt
