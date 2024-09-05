5d2257ba65143fc295e14ecb762a4fb5a23faa8d
Walaakhkh <Walaakhalil98@gmail.com>
2024-09-04 20:38:16 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,33 +1,5 @@
 -- This script lists all privileges of the users 'user_0d_1' and 'user_0d_2'
 
 -- Check if 'user_0d_1' and 'user_0d_2' have any schema-level privileges
-SELECT 
-    GRANTEE AS 'User',
-    'SCHEMA_PRIVILEGES' AS 'Privilege Type',
-    PRIVILEGE_TYPE AS 'Privilege'
-FROM 
-    information_schema.SCHEMA_PRIVILEGES
-WHERE 
-    GRANTEE IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'")
-UNION ALL
-
--- Check if 'user_0d_1' and 'user_0d_2' have any table-level privileges
-SELECT 
-    GRANTEE AS 'User',
-    'TABLE_PRIVILEGES' AS 'Privilege Type',
-    PRIVILEGE_TYPE AS 'Privilege'
-FROM 
-    information_schema.TABLE_PRIVILEGES
-WHERE 
-    GRANTEE IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'")
-UNION ALL
-
--- Check if 'user_0d_1' and 'user_0d_2' have any global user privileges
-SELECT 
-    GRANTEE AS 'User',
-    'USER_PRIVILEGES' AS 'Privilege Type',
-    PRIVILEGE_TYPE AS 'Privilege'
-FROM 
-    information_schema.USER_PRIVILEGES
-WHERE 
-    GRANTEE IN ("'user_0d_1'@'localhost'", "'user_0d_2'@'localhost'");
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
