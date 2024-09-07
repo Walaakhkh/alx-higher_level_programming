9981471e3ba99b34efa1e94b12d4ae7111b6abe6
Walaakhkh <Walaakhalil98@gmail.com>
2024-09-07 03:25:32 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,10 +7,10 @@ import requests
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    
+
     # Send a GET request to the URL
     response = requests.get(url)
-    
+
     # Display the body response in the specified format
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
